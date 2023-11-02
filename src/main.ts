00f4cb05e9dfe252d0d3ab80ee989348c7c9378c
kaique64 <kaiquehenrique.2005@outlook.com>
2023-11-02 20:13:26 -0300

    delete example test and change main file to typescript file

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { Quasar, Notify, Dialog } from 'quasar';
+import piniaPersist from 'pinia-plugin-persist';
+
+import App from './App.vue';
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
+
+const app = createApp(App);
+
+const pinia = createPinia();
+pinia.use(piniaPersist);
+
+app.use(pinia);
+
+app.use(Quasar, {
+  plugins: {
+    Notify,
+    Dialog,
+  },
+});
+
+app.use(VueAxios, axios);
+app.provide('axios', app.config.globalProperties.axios);
+app.mount('#app');
