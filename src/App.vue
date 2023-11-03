e6f1fce1ff99ca28e8590a3198a549439060c5e2
COK1CA <kaique.costa@br.bosch.com>
2023-11-03 20:11:12 -0300

    add default layout style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <RouterView />
+  <q-layout
+    view="hHh LPR lFf"
+    class="font-regular-bosch bg-white"
+    style="min-height:60vh;"
+  >
+    <RouterView />
+  </q-layout>
 </template>
 
 <script setup lang="ts"></script>
