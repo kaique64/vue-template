d86677c8834c91ab788b79e08db42fbf5e343a1d
COK1CA <kaique.costa@br.bosch.com>
2023-11-03 19:44:03 -0300

    change main file

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,87 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { Quasar, Notify, Dialog } from 'quasar';
-import piniaPersist from 'pinia-plugin-persist';
-
 import App from './App.vue';
-import './index.css';
+import router from './router.js';
+import { Buffer } from 'buffer';
 
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-
+// Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css';
+// Import Quasar css
 import 'quasar/src/css/index.sass';
 
-const app = createApp(App);
+import { AppFullscreen, Loading, LocalStorage, Notify, Quasar } from 'quasar';
+import '@quasar/extras/mdi-v6/mdi-v6.css';
 
-const pinia = createPinia();
-pinia.use(piniaPersist);
+import AuthService from './common/service/integration/AuthService';
+import NoAccessPage from '@/pages/NoAccessPage.vue';
 
-app.use(pinia);
+const run = async () => {
+	const authService = new AuthService();
+	if (!authService.isAuthenticated) {
+		const response = await authService.instance.handleRedirectPromise();
+		if (response !== null) {
+			authService.loginWithResponse(response);
+		} else {
+			await authService.login();
+		}
+	}
 
-app.use(Quasar, {
-  plugins: {
-    Notify,
-    Dialog,
-  },
-});
+	const app = createApp(
+		!(await authService.isAuthorized()) ? NoAccessPage : App
+	);
 
-app.use(VueAxios, axios);
-app.provide('axios', app.config.globalProperties.axios);
-app.mount('#app');
+	const config = {
+		plugins: { Notify, LocalStorage, AppFullscreen, Loading } // import Quasar plugins and add here
+	};
+
+	const global = window;
+	global.Buffer = Buffer;
+	window.process = {
+		env: {
+			NODE_ENV: import.meta.env.NODE_ENV
+		}
+	};
+	const pinia = createPinia();
+
+	const modules = import.meta.globEager('/src/modules/*.ts');
+
+	// install all modules under `modules/`
+	// @ts-ignore
+	Object.values(modules).forEach((module) => module.install?.(app));
+
+	// In case of redeployed frontend the currently active users might have problems to dynamically load JS modules.
+	// For that problem we catch the resulting error and force a reloading of the page.
+	window.addEventListener('unhandledrejection', function(event) {
+		console.log(event);
+
+		if (event instanceof PromiseRejectionEvent && event.isTrusted) {
+			const messageToCheck = event.reason.toString();
+			console.log(messageToCheck);
+
+			const messagesToReload = [
+				'TypeError: Failed to fetch dynamically imported module',
+				'Error: Unable to preload CSS for',
+				'TypeError: error loading dynamically imported module'
+			];
+
+			messagesToReload.forEach((messageToReload) => {
+				console.log(
+					'checking for reloadMessage starting with: ' + messageToReload
+				);
+				if (
+					messageToCheck.toLowerCase().startsWith(messageToReload.toLowerCase())
+				) {
+					console.log('force reloading of the page...');
+					window.location.reload();
+				}
+			});
+		}
+	});
+
+	app.use(pinia);
+	app.use(router);
+	app.use(Quasar, config);
+	app.mount('#app');
+}
+
+run();
